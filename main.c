4cdffd0cc62ff480b2c1fa3e428c691a3e00aa38
Razan Srour <razanmsrour@gmail.com>
2024-11-22 17:57:21 +0000

    Add no parameter checker

--- main.c
+++ main.c
@@ -1,1 +1,11 @@
+#include "push_swap.h"
 
+int main(int argc, char **argv)
+{
+    if (argc == 1)
+        return (0);
+
+    // In case of error, it must display "Error" followed by a '\n' on the standard error.
+    // Errors include for example: some arguments aren't integers, some arguments are bigger
+    // than an integer and/or there are duplicates.
+}

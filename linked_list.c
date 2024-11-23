69c400fb9c2a85aa5656e2270e50f45ee4c89ec2
Razan Srour <razanmsrour@gmail.com>
2024-11-23 12:18:08 +0300

    create linked list node creator

--- linked_list.c
+++ linked_list.c
@@ -1,3 +1,38 @@
 #include "push_swap.h"
 
-int create_node()
+t_list	*create_node(int content)
+{
+	t_list	*node;
+
+	node = malloc(sizeof(t_list));
+	if (!node)
+		return (NULL);
+	node->content = content;
+	node->next = NULL;
+	return (node);
+}
+
+int	main(void)
+{
+	t_list *root;
+
+	root = create_node(15);
+	// root.next = malloc(sizeof(t_list));
+	// root.next->content = 2;
+	// root.next->next = malloc(sizeof(t_list));
+	// root.next->next->content = 23;
+	// root.next->next->next = NULL;
+
+	t_list *curr = root;
+
+	while (curr != NULL)
+	{
+		printf("Node value: %d\n", curr->content);
+		curr = curr->next;
+	}
+
+	// free(root.next->next);
+	// free(root.next);
+	free(root);
+	return (0);
+}

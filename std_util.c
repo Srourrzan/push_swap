425a0a2b063129fe9857c5393c62bca7bb1179e7
Razan Srour <razanmsrour@gmail.com>
2024-11-28 20:00:12 +0300

    Revert "create ft_atoi with error handler"
    This reverts commit 7d3524e989943a73a335d77b8423ca7023a14cc1.

--- std_util.c
+++ std_util.c
@@ -6,37 +6,8 @@
 /*   By: rsrour <rsrour@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 19:35:41 by rsrour            #+#    #+#             */
-/*   Updated: 2024/11/28 19:56:53 by rsrour           ###   ########.fr       */
+/*   Updated: 2024/11/28 19:51:07 by rsrour           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
-int	ft_atoi(char *src)
-{
-	int	iter;
-	int	number;
-	int	sign;
-
-	iter = 0;
-	sign = +1;
-	number = 0;
-	if (src[iter] == '-' || src[iter] == '+')
-	{
-		if (src[iter] == '-')
-			sign = -1;
-		iter++;
-	}
-	while (src[iter])
-	{
-		if (src[iter] >= '0' || src[iter] <= '9')
-		{
-			number = (number * 10) + (src[iter] - '0');
-		}
-		else
-			return (0); // create a condition in the call statement to check if
-		iter++;         // len(src) > 1 && ft_atoi(src) == 0 => display "Error" followed by new line
-	}
-	number = number * sign;
-	return (number);
-}

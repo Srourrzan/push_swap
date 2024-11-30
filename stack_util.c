9f3e6f388af98105857c4e2341d8cf0d5569f13e
Razan Srour <razanmsrour@gmail.com>
2024-11-30 16:47:16 +0300

    add rules concernd about filling the stack

---
+++ stack_util.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stack_util.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rsrour <rsrour@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/30 15:49:02 by rsrour            #+#    #+#             */
+/*   Updated: 2024/11/30 16:45:48 by rsrour           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* 
+ * Main Push Swap RULES:
+ * "push_swap" program should take as an argument the stack 'a' formatted as a list of integers.
+ * The first argument should be at the top of the stack.
+*/
+
+#include "push_swap.h"
+
+void    ft_fill_stack(t_list **root, char **numbers)
+{
+    
+}
